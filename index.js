19b2a3827bf2e971035edf0c99c00fbfe63bbabc
meshal <imeshalfm@gmail.com>
2024-10-09 11:32:55 +0300

    complete task
    Co-authored-by: Yousef Almesaeed <yousefalm1@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 	Q1) Create an object called person with properties name, age, and city.
  Set appropriate values for these properties.
 ********************************/
+// Meshal
+const person = {
+  name: 'John Doe',
+  age: 99,
+  city: 'Portland',
+};
 
 /******************************
 	Q2) Add a new key email to the person object and assign it an email address.
 ********************************/
+// Yousef
+person.email = 'john@gmail.com';
 
 /******************************
 	Q3) Write a JavaScript function called hasKey that takes two arguments:
@@ -14,6 +22,8 @@
  { name: 'John', age: 25 }, and the key is 'name', the function should return true,
  but if the key is 'city', it should return false.
 ********************************/
+// M
+const hasKey = (obj, key) => obj.hasOwnProperty(key);
 
 const movies = [
   {
@@ -47,17 +57,39 @@ const movies = [
     genre: 'Drama',
   },
 ];
+
 /******************************
 	Q4) Create a function that accepts an array of objects called 'movies'
 and iterates through it to print the titles of all the movies.
 ********************************/
+// Y
+const printMovies = (movies) => movies.forEach((movie) => console.log(movie.title));
 
 /******************************
 	Q5) Create a function that accepts an array of objects called 'movies'
 and iterates through it to count how many movies were released in the year 1994.
 ********************************/
+// M
+const moviesIn1994 = (movies) => movies.filter((movie) => movie.year === 1994).length;
 
 /******************************
 	Q6) Write a function that updates the genre of the movie "The Dark Knight"
  to "Action/Drama" and returns the modified array of movies.
 ********************************/
+
+// Y
+
+const changeGenreSimple = (movies) =>
+  movies.map((movie) => {
+    if (movie.title === 'The Dark Knight') {
+      movie.genre = 'Action/Drama';
+    }
+    return movie;
+  });
+
+// Y AND M
+const changeGenre = (movies) =>
+  movies.map((movie) => {
+    movie.title === 'The Dark Knight' ? (movie.genre = 'Action/Drama') : null;
+    return movie;
+  });
